d9f84fec95a1ed4275295d79cabecedb559823d4
yasu <101958049+y9su@users.noreply.github.com>
2022-04-11 23:44:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import json
 from mojang import MojangAPI
 import time
 from os import system
-import colorama
-from colorama import init, Fore
-colorama.init()
 
 
 while True:
@@ -27,19 +24,20 @@ while True:
 
 
     losses = data["player"]["stats"]["Duels"].get("sumo_duel_losses")
-    wins = data["player"]["stats"]["Duels"]["sumo_duel_wins"]
-    roundsplayed = data["player"]["stats"]["Duels"]["sumo_duel_rounds_played"]
+    wins = data["player"]["stats"]["Duels"].get("sumo_duel_wins", 0)
+    roundsplayed = data["player"]["stats"]["Duels"].get("sumo_duel_rounds_played", 0)
     wlr = wins if losses is None else round((wins/losses), 2)
 
 
     if losses is None:
-        print(Fore.RED + f"-> {username} has 0 sumo losses" + "\n")
+        losses1 = f"**{username}** has __***0***__ sumo losses" + "\n"
     else:
         losses1 = f"**{username}** has __***{losses}***__ sumo losses" + "\n"
     wins1 = f"**{username}** has __***{wins}***__ sumo wins" + "\n"
     roundsplayed1 = f"**{username}** has played __***{roundsplayed}***__ games of sumo" + "\n"
     wlr1 = f"**{username}** has a __***{wlr}***__ WLR" + "\n"
     
+
     data["embeds"] = [
     {
         "description" : f"{losses1}{wins1}{roundsplayed1}{wlr1}",
@@ -50,4 +48,3 @@ while True:
     print("Stats sent to webhook!")
     time.sleep(10)
     system("cls")
-
